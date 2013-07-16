5995866a1ac6e0349f27054b98bf63f35f5c37be
MistPhizzle <mistphizzle@gmail.com>
2013-07-15 22:50:34 -0400

    Make Command Messages configurable.

--- src/com/etriacraft/tamemanagement/TameManagement.java
+++ src/com/etriacraft/tamemanagement/TameManagement.java
@@ -62,6 +62,18 @@ public class TameManagement extends JavaPlugin {
 		MobListener.doesNotOwn = colorize(getConfig().getString("messages.listener.doesNotOwn"));
 		MobListener.animalReleased = colorize(getConfig().getString("messages.listener.animalReleased"));
 		MobListener.animalTransferred = colorize(getConfig().getString("messages.listener.animalTransferred"));
+		Commands.noPermission = colorize(getConfig().getString("messages.commands.noPermission"));
+		Commands.setVariant = colorize(getConfig().getString("messages.commands.setVariant"));
+		Commands.setColor = colorize(getConfig().getString("messages.commands.setColor"));
+		Commands.claim = colorize(getConfig().getString("messages.commands.claim"));
+		Commands.invoked = colorize(getConfig().getString("messages.commands.invoked"));
+		Commands.configReloaded = colorize(getConfig().getString("messages.commands.configReloaded"));
+		Commands.releaseNotAllowed = colorize(getConfig().getString("messages.commands.releaseNotAllowed"));
+		Commands.transfersNotAllowed = colorize(getConfig().getString("messages.commands.transfersNotAllowed"));
+		Commands.releaseMessage = colorize(getConfig().getString("messages.commands.releaseMessage"));
+		Commands.playerNotOnline = colorize(getConfig().getString("messages.commands.playerNotOnline"));
+		Commands.cantTransferOwnershipToSelf = colorize(getConfig().getString("messages.commands.cantTransferOwnershipToSelf"));
+		Commands.transferMessage = colorize(getConfig().getString("messages.commands.transferMessage"));
 	}
 
 	public static TameManagement getInstance() {
@@ -81,8 +93,19 @@ public class TameManagement extends JavaPlugin {
 		getConfig().addDefault("Breeding.Horse", true);
 		getConfig().addDefault("Breeding.Wolf", true);
 		getConfig().addDefault("Breeding.Ocelot", true);
-
 		getConfig().addDefault("messages.Prefix", "&7[&6TameManagement&7] ");
+		getConfig().addDefault("messages.commands.noPermission", "&cYou do not have permission to do that.");
+		getConfig().addDefault("messages.commands.setVariant", "&aRight click the horse that you would like to change to &3%variant.");
+		getConfig().addDefault("messages.commands.setColor", "&aRight click the horse to change its color to &3%color&a.");
+		getConfig().addDefault("messages.commands.claim", "&aRight click the horse that you would like to claim.");
+		getConfig().addDefault("messages.commands.invoked", "&aYou have invoked &3%amount %mob");
+		getConfig().addDefault("messages.commands.configReloaded", "&aConfig reloaded.");
+		getConfig().addDefault("messages.commands.releaseNotAllowed", "&cThis server does not allow animals to be released.");
+		getConfig().addDefault("messages.commands.transfersNotAllowed", "&cThis server does not allow transferring of animal ownership.");
+		getConfig().addDefault("messages.commands.releaseMessage", "&aYou have released this animal to the wild.");
+		getConfig().addDefault("messages.commands.playerNotOnline", "&cThat player is not online.");
+		getConfig().addDefault("messages.commands.cantTransferOwnershipToSelf", "&cYou can't transfer animal ownership to yourself.");
+		getConfig().addDefault("messages.commands.transferMessage", "&aOwnership of this animal transferred to &3%newowner&a.");
 		getConfig().addDefault("messages.listener.animalDoesNotBelongToYou", "&cYou cant damage an animal that doesnt belong to you.");
 		getConfig().addDefault("messages.listener.horseAlreadyOwned", "&cThis horse is already owned by &3%owner&c.");
 		getConfig().addDefault("messages.listener.cantInteractWithHorse", "&cYou cant interact with a horse belonging to &3&owner&c.");
